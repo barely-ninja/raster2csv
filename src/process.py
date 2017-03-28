e3646a2902a8878ce032c7464c340b7bf16e04eb
Kirill Prostyakov <kprostyakov@gmail.com>
2017-03-28 18:59:56 +0300

    tested with two images

--- src/process.py
+++ src/process.py
@@ -28,11 +28,11 @@ def main(args):
     image = imread(config['input_file'])
     x_ign = config['ignore']['x']
     y_ign = config['ignore']['y']
-
     px_x = config['x_axis']['pixels']
     px_y = config['y_axis']['pixels']
     u_x = config['x_axis']['shown_units']
     u_y = config['y_axis']['shown_units']
+
     pix_offset = np.array([px_y[0], px_x[0]])
     units_offset = np.array([u_y[0], u_x[0]])
     pix_scale = np.array([(u_y[1]-u_y[0])/(px_y[1]-px_y[0]),
@@ -48,19 +48,40 @@ def main(args):
         result = match_template(img_strip, kernel, pad_input=True)
 
         res_mask = np.ones_like(result, dtype=np.bool)
-        res_mask[series['y_range'][0]:series['y_range'][1], px_x[0]-offset-1:px_x[1]+offset] = False
+        try:
+            x_lim = config['x_limits']
+        except KeyError:
+            x_lim = [px_x[0]-offset-1, px_x[1]+offset]
+        try:
+            series_range = series['y_range']
+            res_mask[series_range[1]:series_range[0], x_lim[0]:x_lim[1]] = False
+        except KeyError:
+            res_mask[px_y[1]:px_y[0], x_lim[0]:x_lim[1]] = False
         res_mask[y_ign[0]:y_ign[1], x_ign[0]:x_ign[1]] = True
         masked_result = np.ma.masked_array(result, mask=res_mask)
 
-        coords = peak_local_max(masked_result, min_distance=1, threshold_rel=0.5)
+        coords = peak_local_max(masked_result, min_distance=1, threshold_rel=0.3, num_peaks=15)
+
+        try:
+            manual_picks = np.array([[x[1], x[0]] for x in series['manual']])
+            coords = np.append(coords, manual_picks, axis=0)
+        except KeyError:
+            pass
+        out_fn = config['output_prefix']+'_'.join(series['name'].split(' '))
         if config['debug'] == "yes":
-            plt.plot(coords[:, 1], coords[:, 0], 'r.')
+            xvals = coords[:, 1]
+            yvals = coords[:, 0]
+            pad = 20
+            plt.plot(xvals, yvals, 'r.')
             plt.imshow(masked_result, cmap='gray')
-            plt.show()
+            plt.xlim([xvals.min()-pad, xvals.max()+pad])
+            plt.ylim([yvals.max()+pad, yvals.min()-pad])
+            plt.savefig(out_fn+'.png')
+            plt.close()
 
         scaled = (coords-pix_offset)*pix_scale + units_offset
-        order = np.argsort(scaled[:,1])
-        out_fn = config['output_prefix']+'_'.join(series['name'].split(' '))+'.tsv'
-        np.savetxt(out_fn, list(zip(scaled[order, 1], scaled[order, 0])), '%.2f', '\t')
+        order = np.argsort(scaled[:, 1])
+        np.savetxt(out_fn+'.tsv', list(zip(scaled[order, 1], scaled[order, 0])), '%.2f', '\t')
+
 if __name__ == "__main__":
     main(argv)
